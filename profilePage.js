8de54add01b701d74778926bb2604c4203d1e8ea
Jerico <jericodcosta95375788@gmail.com>
2023-09-23 15:35:22 +0530

    First commit

---
+++ profilePage.js
@@ -0,0 +1,26 @@
+//if token is null redirect to index page
+if(localStorage.getItem("token") === null)
+{
+    window.open("index.html","_self");
+}
+
+let logoutBtn = document.getElementById("logout");
+let userDetails = document.getElementsByClassName("user-details")[0];
+{/* <p>Full Name : Your Name</p>
+<p>Email : Your Email</p>
+<p>Token : Your Token</p>
+<p>Password : Your password</p> */}
+
+userDetails.innerHTML = `
+<p>Full Name : ${localStorage.getItem("name")}</p>
+<p>Email : ${localStorage.getItem("email")}</p>
+<p>Token : ${localStorage.getItem("token")}</p>
+<p>Password : ${localStorage.getItem("password")}</p>
+`
+
+//Adding event to logout button
+logoutBtn.addEventListener("click",()=>{
+    localStorage.clear();
+    window.open("index.html","_self");
+})
+
---
+++ script.js
@@ -0,0 +1,49 @@
+//To prevent back button to load sign up page
+window.history.forward();
+let p2 =  document.getElementsByName("confirm-password")[0];
+let p1 = document.getElementsByName("password")[0];
+//To validate password
+function validatePass() {
+  if (p1.value != p2.value) {
+    p2.setCustomValidity("Password didn't match");
+
+  } else {
+    p2.setCustomValidity("");
+  }
+}
+
+//To generate token
+function generateToken() {
+  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var token = '';
+  for(var i = 0; i < 16; i++) {
+      token += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return token;
+}
+
+let form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //   confirm(p1,p2);
+  let elements = e.target.children;
+    Array.from(elements).forEach((val) => {
+      if(val.hasAttribute("name") && (val.name !== "confirm-password"))
+      {
+          localStorage.setItem(val.name, val.value);
+          val.value = "";
+      }
+      if(val.name === "confirm-password")
+      {
+        val.value = "";
+      }
+    });
+    localStorage.setItem("token",generateToken());
+    window.open("profilePage.html","_self");
+});
+
+
+//To redirect tab
+function noBack() {
+    window.history.forward();
+}
